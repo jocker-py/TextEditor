7aa72f7b7925bce3c9ad927eeb3884d892a0802b
jocker-py <buladzislau@gmail.com>
2023-03-22 21:41:21 +0300

    feat: add renderThree function

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const renderThree = () => {
+  root.render(
+    <React.StrictMode>
+      <App store={store}/>
+    </React.StrictMode>
+  );
+}
+
+renderThree();
+store.setSubscriber(renderThree);
+store._callSubscriber(store.getState());
 

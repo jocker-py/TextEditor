5bdc5dd50be34be088b54c89414bb7f45d36b0d3
jocker-py <buladzislau@gmail.com>
2023-03-22 21:42:11 +0300

    feat: add uncontrolled textarea element with onChangeMessageText function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import './App.scss';
+import { changeMessageActionCreator } from './store/store';
+import { StoreType } from './store/types';
 
-function App() {
+type AppPropsType = {store: StoreType};
+function App(props: AppPropsType) {
+  const state = props.store.getState();
+  const dispatch = props.store.dispatch.bind(props.store);
+  const value = state.value;
+  const onChangeMessageText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    let text = e.currentTarget.value;
+      dispatch(changeMessageActionCreator(text))
+  }
   return (
     <div className="App">
+      <div>
+        <textarea value={value} onChange={onChangeMessageText} placeholder="Enter your note..."/>
+      </div>
+      <div>
+        <button>add Note</button>
+      </div>
     </div>
   );
 }

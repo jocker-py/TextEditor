11302fa0d3fab02d0c4b3893cfbab02886a8d702
jocker-py <buladzislau@gmail.com>
2023-03-23 21:23:32 +0300

    feat: add ActionCreators for actions

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,7 +3,9 @@ import {ActionType, StoreType } from "./types";
 
 const store: StoreType  = {
   _state: {
-    value: 'asfasfa',
+    newNotesText: '',
+    notes : [],
+    modal : true,
   },
   _subscriber: null,
   getState (){
@@ -16,18 +18,50 @@ const store: StoreType  = {
     this._subscriber && this._subscriber(this._state);
   },
   dispatch(action){
-    if(action.type === TypesAction.changeMessage){
-      this._state.value = action.payload;
+    if(action.type === TypesAction.updateNotesText){
+      this._state.newNotesText = action.payload || '';
+    } else if(action.type === TypesAction.addNewNotes){
+      const newNotes = {
+        id: this._state.notes.length + 1, // TODO: edit this later
+        text: this._state.newNotesText
+      }
+      this._state.newNotesText = '';
+      this._state.notes = [...this._state.notes, newNotes];
+    } else if(action.type === TypesAction.showModal){
+      this._state.modal = true;
+    } else if(action.type === TypesAction.closeModal){
+      this._state.modal = false;
     }
     this._callSubscriber(this._state);
   }
 };
 
-type changeMessageActionCreatorType = (text: string) => ActionType;
-export const changeMessageActionCreator: changeMessageActionCreatorType = (text: string) => {
+type UpdateNotesTextActionCreatorType = (text: string) => ActionType;
+export const updateNotesTextActionCreator: UpdateNotesTextActionCreatorType = (text: string) => {
   return {
-    type: TypesAction.changeMessage,
+    type: TypesAction.updateNotesText,
     payload: text,
+  }
+}
+
+type AddNewNotesActionCreatorType = () => ActionType;
+export const addNewNotesActionCreator: AddNewNotesActionCreatorType = () =>{
+  return {
+    type: TypesAction.addNewNotes
+  }
+}
+
+type ShowModalActionCreatorType = () => ActionType;
+export const showModalActionCreator: ShowModalActionCreatorType  = () => {
+  return {
+    type: TypesAction.showModal
+  }
+}
+
+type CloseModalActionCreatorType = () => ActionType;
+export const closeModalActionCreator: CloseModalActionCreatorType = () => {
+  return {
+    type: TypesAction.closeModal
   }
 }
 

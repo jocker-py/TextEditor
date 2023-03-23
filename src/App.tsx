7a91381d80912f98915f625cabf47c303399624d
jocker-py <buladzislau@gmail.com>
2023-03-23 21:24:48 +0300

    feat: add components Footer, Header, ContentContainer, ModalContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React, { ChangeEvent } from 'react';
 import './App.scss';
-import { changeMessageActionCreator } from './store/store';
+import ContentContainer from './components/Content/ContentContainer';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import ModalContainer from './components/Modal/ModalContainer';
+import Sidebar from './components/Sidebar/Sidebar';
 import { StoreType } from './store/types';
 
 type AppPropsType = {store: StoreType};
 function App(props: AppPropsType) {
-  const state = props.store.getState();
-  const dispatch = props.store.dispatch.bind(props.store);
-  const value = state.value;
-  const onChangeMessageText = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    let text = e.currentTarget.value;
-      dispatch(changeMessageActionCreator(text))
-  }
   return (
     <div className="App">
-      <div>
-        <textarea value={value} onChange={onChangeMessageText} placeholder="Enter your note..."/>
-      </div>
-      <div>
-        <button>add Note</button>
-      </div>
+      <Header/>
+      <ModalContainer store={props.store}/>
+      <ContentContainer store={props.store}/>
+      <Sidebar/>
+      <Footer/>
     </div>
   );
 }

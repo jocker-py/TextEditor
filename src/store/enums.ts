52f759ca878a4239253a99a720a5dfb260fc69bf
jocker-py <buladzislau@gmail.com>
2023-03-23 21:19:45 +0300

    feat: add TypesAction

--- src/store/enums.ts
+++ src/store/enums.ts
@@ -1,3 +1,8 @@
 export enum TypesAction {
-  changeMessage = 'CHANGE-MESSAGE',
+  updateNotesText = 'UPDATE-NOTES-TEXT',
+  addNewNotes = 'ADD-NOTES',
+  removeNotes = 'REMOVE-NOTES',
+  editNotes = 'EDIT-NOTES',
+  showModal = 'SHOW-MODAL',
+  closeModal = 'CLOSE-MODAL',
 };

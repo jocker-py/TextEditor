1ae0fd7164b524993b2e9259b9a5b78a711d23f2
jocker-py <buladzislau@gmail.com>
2023-03-27 12:42:38 +0300

    feat: add modal action and title for it

--- src/store/enums.ts
+++ src/store/enums.ts
@@ -1,8 +1,18 @@
 export enum TypesAction {
   updateNotesText = 'UPDATE-NOTES-TEXT',
   addNewNotes = 'ADD-NOTES',
-  removeNotes = 'REMOVE-NOTES',
-  editNotes = 'EDIT-NOTES',
+  editNote = 'EDIT-NOTE',
   showModal = 'SHOW-MODAL',
   closeModal = 'CLOSE-MODAL',
+  showAddNotesModal = 'SHOW-ADD-NOTES-MODAL',
+  showNote = 'SHOW-NOTE',
+  setNote = 'SET-NOTE',
+  updateNote = 'UPDATE-NOTE',
+  removeNote = 'REMOVE-NOTE',
+  toggleTag = 'TOGGLE-TAG',
 };
+
+export enum ModalTitle {
+  createNewNote = 'Create new Note',
+  editNote = 'Edit note',
+}

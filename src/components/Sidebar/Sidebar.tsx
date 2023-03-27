fd70887e0702880fcbc620190f44285de894dfa2
jocker-py <buladzislau@gmail.com>
2023-03-27 16:45:37 +0300

    refactor: remove words

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
-import { toggleTagActionCreator } from '../../store/store';
+import {resetFilterActionCreator, toggleTagActionCreator } from '../../store/store';
 import { ContainerType } from '../../store/types';
-import './Sidebar.scss';
 
 const Sidebar: FC<ContainerType> = (props) => {
   const state = props.store.getState().sideBar;
@@ -11,30 +10,26 @@ const Sidebar: FC<ContainerType> = (props) => {
     const text = e.currentTarget.innerHTML;
     dispatch(toggleTagActionCreator(text));
   }
+
+  const resetFilter = (e: React.MouseEvent) => {
+    dispatch(resetFilterActionCreator());
+  }
   const tagsElements = [...state.tags].map((tag, idx) => {
         if(state.selectedTags.includes(tag)){
-          return (<div key={idx} className="sidebar__tag--selected" onClick={toggleTag}>{tag}</div>)
+          return (<div key={idx} className="sidebar__tag sidebar__tag--selected" onClick={toggleTag}>{tag}</div>)
         } else {
           return <div key={idx} className="sidebar__tag" onClick={toggleTag}>{tag}</div>
         }
       });
-  debugger;
   return (
     <div className="sidebar">
       <h2>Sidebar</h2>
       <div>
         {tagsElements}
       </div>
-      <ol>
-        <li>Создание, редактирование, просмотр и удаление заметок;</li>
-        <li>Фильтр заметок по тегу;</li>
-        <li>Добавление и удаление тегов из списка.</li>
-        <li>Данные хранить в json формате.</li>
-        <li>Использование CSS препроцессора.</li>
-        <li>Использование TypeScript</li>
-        <li>Залить на гит</li>
-        <li>Залить на netlify</li>
-      </ol>
+      <div>
+        <button onClick={resetFilter} className="sidebar__button"/>
+      </div>
     </div>
   );
 };

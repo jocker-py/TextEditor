e1bf6e9b7af612b91bdbb41bf7abd19b83d01579
jocker-py <buladzislau@gmail.com>
2023-03-27 12:42:16 +0300

    feat: add modal, modalContainer and style for it

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const Modal = (props: any) => {
-  const showModal = (e: React.MouseEvent) => {
+type ModalType = {
+  title: string;
+  showModal: () => void;
+  tags: string[];
+  onUpadteNotesText: (text: string) => void;
+  value: string;
+  addNotes : () => void;
+  closeModal: () => void;
+  showEditNote:() => void;
+  showRemoveNote:() => void;
+  confirmButton: boolean;
+  cancelButton: boolean;
+  removeButton: boolean;
+  editButton: boolean;
+  readOnly : boolean;
+  currentValue: string;
+  currentId: number;
+  currentTags: string[];
+}
+const Modal: FC<ModalType> = ({title, value,  tags, addNotes, showModal,
+                                onUpadteNotesText, closeModal, showEditNote,
+                                showRemoveNote, confirmButton, cancelButton,
+                                editButton,removeButton, readOnly, currentValue}) => {
+  const stopCloseModal = (e: React.MouseEvent) => e.stopPropagation();
+  const onCloseModal = (e: React.MouseEvent) => {
     e.stopPropagation();
-    props.showModal();
+    closeModal();
   }
+  const addNote = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    addNotes();
+    closeModal();
+  }
+  const tagsElements = tags.map((tag, idx) => <span key={tag + idx}> {tag} </span>)
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let text = e.currentTarget.value;
+    onUpadteNotesText(text);
+  }
+  const onEditNote = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    showEditNote();
+  }
+  const onRemoveNote = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    showRemoveNote();
+  }
+  const textAreaElements = <div>
+    <textarea value={value} onChange={onChange} placeholder="Enter your note..."/>
+    <div>{tagsElements}</div></div>
+  const textElement = <p>{currentValue}</p>
   return (
-    <div className="modal__content" onClick={showModal}>
-      modal
+    <div className="modal__content" onClick={stopCloseModal}>
+     <h3 className="modal__title">{title}</h3>
+      {readOnly ?  textElement : textAreaElements}
+      <div className="modal__buttons">
+        {confirmButton && <button className="modal__button-confirm" onClick={addNote}>Confirm</button>}
+        {editButton && <button className="modal__button-edit" onClick={onEditNote}>Edit</button>}
+        {removeButton && <button className="modal__button-remove" onClick={onRemoveNote}>Remove</button>}
+        {cancelButton && <button className="modal__button-cancel" onClick={onCloseModal}>Cancel</button>}
+      </div>
     </div>
   );
 };
--- src/components/Modal/ModalContainer.tsx
+++ src/components/Modal/ModalContainer.tsx
@@ -1,21 +1,65 @@
 import React, { FC } from 'react';
-import {closeModalActionCreator, showModalActionCreator } from '../../store/store';
+import {addNewNotesActionCreator,
+  closeModalActionCreator,
+  editNoteActionCreator,
+  removeNoteActionCreator,
+  showModalActionCreator,
+  updateNoteActionCreator,
+  updateNotesTextActionCreator} from '../../store/store';
 import { ContainerType } from '../../store/types';
 import Modal from './Modal';
 import './ModalContainer.scss';
 
 const ModalContainer: FC<ContainerType> = ({store}) => {
-  const isShow = store.getState().modal;
+  const state = store.getState();
   const dispatch = store.dispatch.bind(store);
+  const onUpadteNotesText = (text: string) => {
+    dispatch(updateNotesTextActionCreator(text))
+  }
+  const addNotes = () => {
+    if(state.content.currentNote.id !== -1){
+      dispatch(updateNoteActionCreator());
+    } else {
+      dispatch(addNewNotesActionCreator());
+    }
+  }
   const closeModal = () => {
     dispatch(closeModalActionCreator());
   }
   const showModal = () => {
     dispatch(showModalActionCreator());
   }
-  return isShow ?
-    <div className="modal__container-show" onClick={closeModal}>
-      <Modal showModal={showModal}/>
+  const editNote = () => {
+    dispatch(editNoteActionCreator());
+  }
+  const removeNote = () => {
+    dispatch(removeNoteActionCreator());
+    closeModal();
+  }
+
+  return state.modal.toShow ?
+    <div>
+      <div className="shadow" onClick={closeModal}>
+      </div>
+      <Modal showModal={showModal}
+             title={state.modal.title}
+             value={state.content.newNote.text}
+             currentValue={state.content.currentNote.text}
+             currentId={state.content.currentNote.id}
+             currentTags={state.content.currentNote.tags}
+             tags={state.content.newNote.tags}
+             onUpadteNotesText={onUpadteNotesText}
+             addNotes={addNotes}
+             closeModal={closeModal}
+             showEditNote={editNote}
+             showRemoveNote={removeNote}
+             confirmButton={state.modal.confirmButton}
+             cancelButton={state.modal.cancelButton}
+             editButton={state.modal.editButton}
+             removeButton={state.modal.removeButton}
+             readOnly={state.modal.readOnly}
+
+      />
     </div> :
     <div className="modal__container-hide" />;
 };
